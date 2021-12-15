b52df64ddc5a1870d45531e26cf91517c0704b34
radiya <radiyak@umich.edu>
2021-12-15 18:34:38 -0500

    m

---
+++ testSelectionSort1.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+public class testSelectionSort1 {
+@Test
+public void test() {
+testPositive();
+testNegative();
+
+}
+    public testSelectionSort1() {
+    }
+
+    public void testPositive(){
+        int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = 9;
+        arr[2] = 7;
+        arr[3] = 10;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 2;
+        Sortedarr[1] = 7;
+        Sortedarr[2] = 8;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+        SelectionSort1 x= new SelectionSort1();
+        x.basicSelectionSort(arr);
+         for(int i=0;i<5;i++) {
+         	assert(arr[i]==Sortedarr[i]);
+         }
+        /** add tests to check for this unit test **/
+    }
+
+    public void testNegative(){
+    	int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+        SelectionSort1 x= new SelectionSort1();
+       x.basicSelectionSort(arr);
+        for(int i=0;i<5;i++) {
+        	assert(arr[i]==Sortedarr[i]);
+        }
+        /** Test data contains negative values only **/
+    }
+
+
+}
